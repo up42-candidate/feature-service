32c896d3eb36f22ede9f7e39cb507909fafd5c5c
up42-candidate <61329576+up42-candidate@users.noreply.github.com>
2020-02-22 18:45:57 +0100

    Make /features test green

--- src/main/java/com/example/feature/repository/FeatureRepository.java
+++ src/main/java/com/example/feature/repository/FeatureRepository.java
@@ -2,9 +2,12 @@ package com.example.feature.repository;
 
 import com.example.feature.model.Feature;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface FeatureRepository {
     Optional<Feature> getById(UUID id);
+
+    List<Feature> getAll();
 }
